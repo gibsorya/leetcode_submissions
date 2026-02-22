4d9525c72c10426a93b5d730bb9dd896db9ee987
Ryan Gibson <ryan@gibs.gg>
2026-02-22 20:50:32 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 57.8 MB (92.72%)

---
+++ my-folder/0026-remove-duplicates-from-sorted-array/solution.ts
@@ -0,0 +1,11 @@
+function removeDuplicates(nums: number[]): number {
+    let insertIndex = 1;
+    for(let i = 1; i < nums.length; i++) {
+        if(nums[i - 1] !== nums[i]) {
+            nums[insertIndex] = nums[i]
+            insertIndex++;
+        }
+    }
+
+    return insertIndex;
+};
