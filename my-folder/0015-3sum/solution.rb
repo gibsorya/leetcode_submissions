61a8b2626a1fb41681c2dc85384ec704dd9b8ac1
Ryan Gibson <ryan@gibs.gg>
2026-03-21 06:21:31 +0000

    [LeetCode Sync] Runtime - 542 ms (70.62%), Memory - 217 MB (37.91%)

--- my-folder/0015-3sum/solution.rb
+++ my-folder/0015-3sum/solution.rb
@@ -4,16 +4,20 @@ def three_sum(nums)
     nums.sort!
     res = []
 
-    for i in 0..(nums.length - 2) do
-        next if i > 0 && nums[i] == nums[i - 1]
-        l = i + 1
+    nums.each_with_index do |num, index|
+        next if index != 0 && num == nums[index - 1]
+        l = index + 1
         r = nums.length - 1
+
         while l < r
-            sum = nums[i] + nums[l] + nums[r]
+            sum = num + nums[l] + nums[r]
             if sum == 0
-                res.push([nums[i], nums[l], nums[r]]) 
+                res << [num, nums[l], nums[r]]
                 l += 1
                 r -= 1
+                while l < r && nums[l] == nums[l - 1]
+                    l += 1
+                end
             elsif sum < 0
                 l += 1
             else
@@ -22,5 +26,5 @@ def three_sum(nums)
         end
     end
 
-    res.uniq
+    res
 end
