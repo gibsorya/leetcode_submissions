e19eefdc698f73879603bcab00d3a088b3c7ba19
Ryan Gibson <ryan@gibs.gg>
2026-03-25 20:59:18 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 211.6 MB (65.67%)

---
+++ my-folder/1528-kids-with-the-greatest-number-of-candies/solution.rb
@@ -0,0 +1,9 @@
+# @param {Integer[]} candies
+# @param {Integer} extra_candies
+# @return {Boolean[]}
+def kids_with_candies(candies, extra_candies)
+    max = candies.max
+    candies.map do |candy|
+        candy + extra_candies >= max
+    end
+end
